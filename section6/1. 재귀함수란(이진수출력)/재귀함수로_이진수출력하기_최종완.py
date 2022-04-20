e40a0395de84d9a9342fa6f09177925d0325382d
ppkk0906 <59152690+ppkk0906@users.noreply.github.com>
2022-04-21 00:03:44 +0900

    6-1  solved

--- section6/1. 재귀함수란(이진수출력)/재귀함수로_이진수출력하기_최종완.py
+++ section6/1. 재귀함수란(이진수출력)/재귀함수로_이진수출력하기_최종완.py
@@ -1,38 +1,52 @@
-import heapq
 import sys
 from time import time
 
 
 pwd = __file__[0:__file__.rfind("/")] # 모듈위치
 
+def binary(num, square, answerArray):
+    divisor = 2 ** square
+
+    if divisor < 1:
+        return
+
+    answerArray.append(str(num // divisor))
+    binary(num % divisor, square - 1, answerArray)
+
+
 for FILE_NO in range(1,6): # i는 파일번호
     START = time()
     sys.stdin = open(pwd + f"/in{FILE_NO}.txt", 'r')
 
 #============================================================================#
-
+    
+    N = int(sys.stdin.readline().rstrip())
     정답 = []
-    tree = []
+    square = 0
 
     while True:
-        command = int(sys.stdin.readline().rstrip()) * -1
+        left = 2 ** square
+        right = 2 ** (square+1)
 
-        if command == 0:
-            정답.append(abs(heapq.heappop(tree)))
-        elif command == 1:
+        if left <= N and N < right:
             break
-        else:
-            # heapq 자체가 자료구조가 아님에 유의
-            heapq.heappush(tree, command)
-        
+
+        square += 1 
     
+    binary(N, square, 정답)
+
+    
+    정답 = ''.join(정답)
+
 #============================================================================#
 
     END = time()
-    REAL_ANSWER = list(map(lambda s: int(s.rstrip()), open(pwd + f"/out{FILE_NO}.txt", 'r').readlines()))
+    REAL_ANSWER = open(pwd + f"/out{FILE_NO}.txt", 'r').readline()
 
     if 정답 == REAL_ANSWER:
         print("정답")
     else:
+        print(정답)
+        print(REAL_ANSWER)
         print("오답")
     print(f"실행시간: {END - START}")
