2e940d1c701b2a9c665c258624f64c34534d3ad5
ppkk0906 <59152690+ppkk0906@users.noreply.github.com>
2022-05-07 23:40:49 +0900

    Update 중위순회_최종완.py

--- section6/2.이진트리순회/중위순회_최종완.py
+++ section6/2.이진트리순회/중위순회_최종완.py
@@ -1,33 +1,79 @@
+class Node():
+    def __init__(self, num):
+        self.num = num
+        self.parent = None
+        self.left = None
+        self.right = None
+    
+    def setParent(self, parent):
+        self.parent = parent
 
+    def setLeft(self, left):
+        self.left = left
+
+    def setRight(self, right):
+        self.right = right
+
+    def getParent(self):
+        return self.parent
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+
+    def getNumber(self):
+        return self.num
+
+    def isRoot(self):
+        return self.getParent() is None
+
+    def isLeaf(self):
+        return self.getLeft() is None and self.getRight() is None
 
 # 그래프 초기화
-graph = [None] * 8
-visitied = [False] * len(graph)
-stack = []
-
-graph[1] = [2, 3]
-graph[2] = [1,4,5]
-graph[3] = [1,3,6,7]
-graph[4] = [2]
-graph[5] = [2]
-graph[6] = [3]
-graph[7] = [3]
-
-#중위순회 실시
-def dfs(start, graph, visitied):
-    if not visitied[start]: print(start, end=' ')
-    visitied[start] = True
-
-    children = list(graph[start]) #순회 대상
-    sorted(children, reverse=True)
-
-    for child in children:
-        if visitied[child]: continue
-        visitied[child] = True
-
-        print(child, end=' ')
-        dfs(child, graph, visitied)
-
+nodes = [Node(n) for n in range(8)]
     
+nodes[0] = None
 
-dfs(4, graph, visitied)
+nodes[1].setLeft(nodes[2])
+nodes[1].setRight(nodes[3])
+
+nodes[2].setParent(nodes[1])
+nodes[2].setLeft(nodes[4])
+nodes[2].setRight(nodes[5])
+
+nodes[3].setParent(nodes[1])
+nodes[3].setLeft(nodes[6])
+nodes[3].setRight(nodes[7])
+
+nodes[4].setParent(nodes[2])
+nodes[5].setParent(nodes[2])
+nodes[6].setParent(nodes[3])
+nodes[7].setParent(nodes[3])
+
+visitied = set()
+visitied.add(None)
+
+def inorder(target, nodes, visitied):
+    if target not in visitied:
+        print(target.getNumber(), end=' ')
+        visitied.add(target)
+    
+    parent = target.getParent()
+    left = target.getLeft()
+    right = target.getRight()
+
+    # 방문할 자식 노드 체크 => 있으면 방문하기
+    if left not in visitied:
+        inorder(left, nodes, visitied)
+    elif right not in visitied:
+        inorder(right, nodes, visitied)
+    
+    # 없으면 부모노드 방문
+    # elif parent not in visitied:
+    if parent is not None:
+        inorder(parent, nodes, visitied)
+
+inorder(nodes[4], nodes, visitied)
